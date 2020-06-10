d7b560f44140590e87ec189f97aa91ad33647b04
maks53855 <maks538552001@gmail.com>
2020-06-10 17:35:19 +0300

    3DGame

---
+++ Assets/NPC.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NPC : MonoBehaviour
+{
+    public Transform monster;
+    public Transform point;
+    public int speed;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(monster.position, point.position, speed);
+    }
+}
